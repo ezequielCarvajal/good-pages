61529cb8721b682eac1f9e79a6d05d063d8abbba
ezequielCarvajal <ezequielcarvajaldev@gmail.com>
2022-06-30 19:47:40 -0600

    3 commit styles and deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCNu3LqM5p5v43xnjBB2q2iB5jOQ2iiGqo",
+    authDomain: "frontend-pages.firebaseapp.com",
+    projectId: "frontend-pages",
+    storageBucket: "frontend-pages.appspot.com",
+    messagingSenderId: "885521662887",
+    appId: "1:885521662887:web:5786afa76a3eed564c6c82",
+    measurementId: "G-758P90YBHK"
+  }
 };
